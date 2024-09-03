2a4c8df3af7036e55084231c551259a753976185
wheeler-cs <wheelercs@protonmail.com>
2024-09-03 10:41:02 -0500

    Merged Setup Scripts Into Single File
    Merged the setup script for building the Docker image into a single
    Linux-compatible Windows Powershell script.

--- docker_setup.ps1
+++ docker_setup.ps1
@@ -1,3 +1,8 @@
+#!/bin/bash
+# This works on Windows _AND_ Linux
 
+# Build the image
 docker build -t xailab .
+
+# Run the lab
 docker run --name xai -p 8888:8888 xailab
--- docker_setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t xailab .
-sudo docker run --name xai -p 8888:8888 xailab
